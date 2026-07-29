152da6a8eac4f6a4429088a7c6e19981575e7e73
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2026-07-28 22:19:46 -0300

    feat: implement OpenRouterService and integrate with server; add configuration and environment setup

--- module-02-integracao-api-llms/01-smart-model-router-gateway/src/index.ts
+++ module-02-integracao-api-llms/01-smart-model-router-gateway/src/index.ts
@@ -1,6 +1,12 @@
+import { config } from './config.ts';
+import { OpenRouterService } from './openrouterService.ts';
 import { createServer } from './server.ts';
 
-const app = createServer();
+
+const routerService = new OpenRouterService(config);
+
+
+const app = createServer(routerService);
 
 await app.listen({ port: 3000, host: '0.0.0.0' })
 console.log(`Server is running at http://localhost:3000`);
@@ -8,7 +14,7 @@ console.log(`Server is running at http://localhost:3000`);
 app.inject({
     method: "POST",
     url: "/chat",
-    body: { question: "Hello World"}
+    body: { question: "What is rate limiting?"}
 }).then((response) => {
     console.log("Response status:", response.statusCode);
     console.log("Response body:", response.body);
