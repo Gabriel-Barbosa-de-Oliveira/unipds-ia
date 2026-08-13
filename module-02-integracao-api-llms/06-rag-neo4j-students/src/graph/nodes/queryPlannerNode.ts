362905d94e8898f7d26fca4cb5bf2b1b105f0df2
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2026-08-13 19:50:02 -0300

    Wip: Passing Messages Foward. Formatting Queries

--- module-02-integracao-api-llms/06-rag-neo4j-students/src/graph/nodes/queryPlannerNode.ts
+++ module-02-integracao-api-llms/06-rag-neo4j-students/src/graph/nodes/queryPlannerNode.ts
@@ -1,3 +1,4 @@
+import { getSystemPrompt, getUserPromptTemplate, QueryAnalysisSchema } from '../../prompts/v1/queryAnalyzer.ts';
 import { OpenRouterService } from '../../services/openrouterService.ts';
 import type { GraphState } from '../graph.ts';
 
@@ -6,6 +7,41 @@ export function createQueryPlannerNode(llmClient: OpenRouterService) {
   return async (state: GraphState): Promise<Partial<GraphState>> => {
 
     try {
+      const systemPrompt = getSystemPrompt();
+      const userPrompt = getUserPromptTemplate(state.question!);
+
+      const { data, error } = await llmClient.generateStructured(
+        systemPrompt,
+        userPrompt,
+        QueryAnalysisSchema,
+      )
+
+      if (error) {
+        console.log(`Failed to analyze query, assuming simple`);
+        return {
+          ...state,
+          error,
+          isMultiStep: true,
+
+        }
+      }
+
+      if(data?.requiresDecomposition && !!data.subQuestions?.length) {
+
+        const subQuestionsFormatted = data.subQuestions
+          .map((subQuestion: string, index: number) => `\n ${index + 1}. ${subQuestion}`)
+          .join('');
+
+        console.log(`Complex Query - ${data.subQuestions.length} steps:${subQuestionsFormatted}`);
+
+        return {
+          isMultiStep: true,
+          subQuestions: data.subQuestions,
+          currentStep: 0,
+          subQueries: [],
+          subResults: []
+        }
+      }
 
       return {
         ...state,
