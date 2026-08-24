2ec5ba914e2d4c9f207ec7239adb4483960c506f
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2026-08-24 15:44:44 -0300

    Feat: Implement customer management tools and prompts

--- module-03-mcp-na-pratica/06-your-legacy-api-as-mcp/customers-mcp/src/mcp/server.ts
+++ module-03-mcp-na-pratica/06-your-legacy-api-as-mcp/customers-mcp/src/mcp/server.ts
@@ -1,8 +1,12 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
-import { CustomerHttpClient } from "../infrastructure/customerHttpClient.ts";
 import { registerListCustomersTool } from "./tools/listCustomers.ts";
 import { CustomerService } from "../application/customerService.ts";
 import { registerApiInfoResource } from "./resources/apiInfo.ts";
+import { registerCreateCustomersTool } from "./tools/createCustomer.ts";
+import { registerGetCustomerTool } from "./tools/getCustomer.ts";
+import { registerFindCustomerPrompt } from "./prompts/findCustomer.ts";
+import { registerUpdateCustomersTool } from "./tools/updateCustomer.ts";
+import { registerDeleteCustomersTool } from "./tools/deleteCustomer.ts";
 
 const BASE_URL = "http://localhost:9999/v1";
 const service = new CustomerService(BASE_URL)
@@ -13,4 +17,9 @@ export const server = new McpServer({
 });
 
 registerListCustomersTool(server, service)
+registerGetCustomerTool(server, service)
+registerCreateCustomersTool(server, service)
+registerFindCustomerPrompt(server)
 registerApiInfoResource(server, BASE_URL)
+registerUpdateCustomersTool(server, service)
+registerDeleteCustomersTool(server, service)
